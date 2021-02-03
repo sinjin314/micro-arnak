eab2bf1df3d04ff1cf22b235d37e837cd5b76930
abdelhadi hasnaoui <abdelhdai.hasnaoui@protonmail.com>
2021-02-03 01:27:08 +0100

    sidebar guest

---
+++ resources/views/guest/sidebar.blade.php
@@ -0,0 +1,28 @@
+<!-- Sidebar (immediate child of the page wrapper) -->
+<div class="sidebar">
+    <div class="sidebar-menu">
+        <!-- Sidebar content with the search box -->
+        <div class="sidebar-content">
+            <input type="text" class="form-control" placeholder="Search">
+            <div class="mt-10 font-size-12"> <!-- mt-10 = margin-top: 1rem (10px), font-size-12 = font-size: 1.2rem (12px) -->
+                <button class="btn btn-success mr-5" type="submit">search</button>
+            </div>
+        </div>
+
+
+        <h5 class="sidebar-title" > helloà </h5>
+        <a class="btn btn-success mr-5" href="{{ route('games.index') }}" >Enter on site</a>
+        <div class="sidebar-divider"></div>
+        <a href="#" class="sidebar-link active">Home</a>
+        <a href="#" class="sidebar-link">Your cart</a>
+        <a href="#" class="sidebar-link">Your account</a>
+        <a href="#" class="sidebar-link">Messenger</a>
+        <br />
+        <h5 class="sidebar-title">MicroArnak</h5>
+        <div class="sidebar-divider"></div>
+        <a href="#" class="sidebar-link">File explorer</a>
+        <a href="#" class="sidebar-link">Spreadsheet</a>
+        <a href="#" class="sidebar-link">Map</a>
+
+    </div>
+</div>
